93a6db2549cab22f44106ac2be8ae39fb24f137a
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-13 15:42:53 +0700

    Change header from tab bars to toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
+import { About } from './components/pages/about/about';
+import { Experience } from './components/pages/experience/experience';
+import { SkillsTools } from './components/pages/skills-tools/skills-tools';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'about',
+        component: About
+    },
+    {
+        path: 'experience',
+        component: Experience
+    },
+    {
+        path: 'skills-tools',
+        component: SkillsTools
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink } from '@angular/router';
+import { Home } from "../pages/home/home";
+import { About } from "../pages/about/about";
+import { Experience } from "../pages/experience/experience";
+import { SkillsTools } from "../pages/skills-tools/skills-tools";
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [MatTabsModule],
+  imports: [MatToolbar, MatButtonModule, RouterLink, Home, About, Experience, SkillsTools],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
