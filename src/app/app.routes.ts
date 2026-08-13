4ff66321775379dce7359f27a0cd59d871c522ba
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-13 22:59:23 +0700

    Change path to main-page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
+import { MainPage } from './components/pages/main-page/main-page';
 
 export const routes: Routes = [
     {
-        path: 'home',
-        component: Home
+        path: 'portfolio',
+        component: MainPage
     },
     {
         path: '',
-        redirectTo: 'home',
+        redirectTo: 'portfolio',
         pathMatch: 'full',
     },
     {
         path: '**',
-        redirectTo: 'home'
+        redirectTo: 'portfolio'
     }
 ];
