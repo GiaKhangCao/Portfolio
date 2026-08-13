7cd476abdf082acc9bb0138197bdd8db4836a9c4
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-13 22:35:07 +0700

    Implementing home section and change from routing to scrolling page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
-import { About } from './components/pages/about/about';
-import { Experience } from './components/pages/experience/experience';
-import { SkillsTools } from './components/pages/skills-tools/skills-tools';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: Home
-    },
-    {
-        path: 'about',
-        component: About
-    },
-    {
-        path: 'experience',
-        component: Experience
-    },
-    {
-        path: 'skills-tools',
-        component: SkillsTools
     },
     {
         path: '',
--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatAnchor } from "@angular/material/button";
+import { About } from '../about/about';
+import { Experience } from '../experience/experience';
+import { SkillsTools } from '../skills-tools/skills-tools';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatAnchor, About, Experience, SkillsTools],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
