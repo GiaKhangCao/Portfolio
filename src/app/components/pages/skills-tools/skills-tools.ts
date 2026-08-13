d4781cccd4fc318e8ba1751f882924bbfba29958
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-13 14:31:17 +0700

    Generate components

---
+++ src/app/components/pages/skills-tools/skills-tools.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-tools',
+  imports: [],
+  templateUrl: './skills-tools.html',
+  styleUrl: './skills-tools.scss',
+})
+export class SkillsTools {
+
+}
