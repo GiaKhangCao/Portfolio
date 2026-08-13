d4d0f986ed071379106ec5281844ea9cd793ac52
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-13 14:45:16 +0700

    Implement header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
