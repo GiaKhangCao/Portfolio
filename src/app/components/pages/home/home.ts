38f61e5a5cebc482f30c1058fcb6b3ebe2e286e8
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-14 17:38:54 +0700

    Change Introduction section to card layout and change button to outlied style

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { MatAnchor } from "@angular/material/button";
-import { About } from '../about/about';
-import { Experience } from '../experience/experience';
-import { SkillsTools } from '../skills-tools/skills-tools';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [MatAnchor, About, Experience, SkillsTools],
+  imports: [MatAnchor, MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
