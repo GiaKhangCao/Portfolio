81cf22a7afa050e5b8e0d85b8106a3434071671f
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-14 18:23:32 +0700

    Add content to introduction section

--- src/app/components/sections/introduction/introduction.ts
+++ src/app/components/sections/introduction/introduction.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatAnchor } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-introduction',
-  imports: [MatAnchor, MatCardModule],
+  imports: [MatAnchor, MatCardModule,MatGridListModule, MatListModule, MatDividerModule],
   templateUrl: './introduction.html',
   styleUrl: './introduction.scss',
 })
