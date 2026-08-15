85a2f0004ade676f5ad362addfa40c7cc1374e3f
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 22:10:25 +0700

    Initiate contact component and add it to main page

--- src/app/components/main-page/main-page.ts
+++ src/app/components/main-page/main-page.ts
@@ -4,10 +4,11 @@ import { SkillsTools } from "../sections/skills-tools/skills-tools";
 import { Introduction } from '../sections/introduction/introduction';
 import { ExperienceComponent } from '../sections/experience-component/experience-component';
 import { PersonalProject } from '../sections/personal-project/personal-project';
+import { Contact } from '../sections/contact/contact';
 
 @Component({
   selector: 'app-main-page',
-  imports: [ About, SkillsTools, Introduction, ExperienceComponent, PersonalProject],
+  imports: [ About, SkillsTools, Introduction, ExperienceComponent, PersonalProject, Contact],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
