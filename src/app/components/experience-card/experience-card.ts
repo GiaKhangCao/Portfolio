7c053ee0515eebc4dc13718b9d2ff1e0476b0d0a
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 10:27:33 +0700

    Implementing experience-card

--- src/app/components/experience-card/experience-card.ts
+++ src/app/components/experience-card/experience-card.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Experience } from '../../types/experience';
 
 @Component({
   selector: 'app-experience-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience-card.html',
   styleUrl: './experience-card.scss',
 })
 export class ExperienceCard {
-
+  @Input({required: true}) experience!: Experience;
 }
