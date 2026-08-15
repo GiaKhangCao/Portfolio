da3f2843a66a68136939364f9be9c6881997ff3a
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 21:56:31 +0700

    Implement personal project section

--- src/app/components/sections/personal-project/personal-project.ts
+++ src/app/components/sections/personal-project/personal-project.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ProjectModel } from '../../../models/ProjectModel';
+import { ProjectCard } from '../../project-card/project-card';
 
 @Component({
   selector: 'app-personal-project',
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './personal-project.html',
   styleUrl: './personal-project.scss',
 })
 export class PersonalProject {
-
+  projects: ProjectModel[] = [
+    {
+      name: 'To Do Web App',
+      description: 'Task manager with creation, toggling, and deletion',
+      url: 'https://github.com/GiaKhangCao/To-Do-App',
+      usingTools: [
+        'React', 'TypeScript', 'CSS'
+      ]
+    },
+  ]
 }
