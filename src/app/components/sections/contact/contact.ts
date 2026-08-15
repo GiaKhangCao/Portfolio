6aeb9a73e1cfbc91994ebc853efe620edee3f24f
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 22:18:46 +0700

    Implement contact section

--- src/app/components/sections/contact/contact.ts
+++ src/app/components/sections/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {  MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
