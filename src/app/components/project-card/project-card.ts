be9c5558525934ba48175c0b83a86a9a39de8f72
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 21:39:01 +0700

    Implement project card

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProjectModel } from '../../models/ProjectModel';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './project-card.html',
   styleUrl: './project-card.scss',
 })
 export class ProjectCard {
-
+ @Input({required: true}) project!: ProjectModel;
 }
