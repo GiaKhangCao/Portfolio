8429678fe49924c26684caed224bcdc22e24e777
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 21:42:10 +0700

    Add button to project repo

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ProjectModel } from '../../models/ProjectModel';
 import { MatCardModule } from '@angular/material/card';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-project-card',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatAnchor],
   templateUrl: './project-card.html',
   styleUrl: './project-card.scss',
 })
