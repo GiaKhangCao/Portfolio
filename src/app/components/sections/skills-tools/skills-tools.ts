cf2b7444a1388b3bb2146293d28fe73d71409dc1
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 17:33:51 +0700

    Implement skill badges

--- src/app/components/sections/skills-tools/skills-tools.ts
+++ src/app/components/sections/skills-tools/skills-tools.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Skill } from '../../../models/skill-model';
+import { Badge } from '../../badge/badge';
 
 @Component({
   selector: 'app-skills-tools',
-  imports: [],
+  imports: [Badge],
   templateUrl: './skills-tools.html',
   styleUrl: './skills-tools.scss',
 })
 export class SkillsTools {
-
+  skills: Skill[] = [
+    {
+      name: 'Angular',
+      iconName: 'Angular'
+    },
+    {
+      name: 'Blazor',
+      iconName: 'Blazor'
+    },
+    {
+      name: 'C#',
+      iconName: 'C#'
+    },
+  ]
 }
