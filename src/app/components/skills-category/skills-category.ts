b17f1b79c0285bd48e9580495681d873729e6464
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 17:44:56 +0700

    Initiate skills-category component

---
+++ src/app/components/skills-category/skills-category.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-category',
+  imports: [],
+  templateUrl: './skills-category.html',
+  styleUrl: './skills-category.scss',
+})
+export class SkillsCategory {
+
+}
