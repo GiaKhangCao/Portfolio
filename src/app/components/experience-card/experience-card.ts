e6ca0be6a88002732e6b4ba3ca089be6786842c7
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 10:52:51 +0700

    Change layout of experience-card

--- src/app/components/experience-card/experience-card.ts
+++ src/app/components/experience-card/experience-card.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Experience } from '../../models/experience';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-experience-card',
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './experience-card.html',
   styleUrl: './experience-card.scss',
 })
