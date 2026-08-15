6ba3e18a3ae1e7386fcd7debca75a15acb799a02
Gia Khang Cao <khang2004caogia@gmail.com>
2026-08-15 17:55:41 +0700

    Implement skill-category component

--- src/app/components/skills-category/skills-category.ts
+++ src/app/components/skills-category/skills-category.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Badge } from '../badge/badge';
+import { SkillCategory } from '../../models/skill-model';
 
 @Component({
   selector: 'app-skills-category',
-  imports: [],
+  imports: [Badge],
   templateUrl: './skills-category.html',
   styleUrl: './skills-category.scss',
 })
 export class SkillsCategory {
-
+  @Input({required: true}) category!: SkillCategory;
 }
